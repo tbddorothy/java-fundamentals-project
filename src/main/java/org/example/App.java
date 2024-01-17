cc7bf2b641e0738ee394acb34e5dde2c41d7595c
Dorothy Mahoko <mahokod@fourier.co.za>
2024-01-17 11:05:20 +0200

    clean

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,12 +3,7 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * This is your main class
- *
- */
 @SpringBootApplication
-//@EnableSwagger2
 public class App 
 {
     public static void main( String[] args )
